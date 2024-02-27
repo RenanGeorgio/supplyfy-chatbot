75a22e3e699e6a916a822b782d1b96c322df0026
RenanGeorgio <regeo9@hotmail.com>
2024-02-27 16:27:18 -0300

    fix

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,41 +1,31 @@
 import { Router } from "express";
-import middlewares from "../middlewares";
+import { authMiddleware  } from "../middlewares";
 
 // server controller
 import * as serverController from "../controllers/server/serverController"; 
 // chat controller
 import * as chatController from "../controllers/chat/chatController";
 // whatsapp controller
-// import * as metaWebhookController from "../controllers/com/whatsapp/webhookController";
-import * as comController from "../controllers/com/whatsapp/comController";
-// facebook controller
-import * as facebook from '../services/facebook';
+import * as whatsappController from "../controllers/com/whatsapp/whatsappController";
 
 const routes = Router();
 
 routes
-    // Test-server
-    .get("/test", serverController.test)
+  // Test-server
+  .get("/test", serverController.test)
 
-    // Meta webhook
-    // .get("/webhook", metaWebhookController.subscribeToWb)
-    // .post("/webhook", metaWebhookController.incomingWb)
+  // chat
+  .post("/chat", authMiddleware.JWT, chatController.create)
+  .get("/chat/:id", authMiddleware.JWT, chatController.list)
 
-    // chat
-    .post("/chat", middlewares.JWT, chatController.create)    
-    .get("/chat/:id", middlewares.JWT, chatController.list)
-
-    // whatsapp plugin
-    .post("/whatsapp/set-status", comController.markMessageAsRead)
-    .post("/whatsapp/send-msg", comController.sendTextMessage)
-    .post("/whatsapp/inte-btn", comController.sendButtonsMessage)
-    .post("/whatsapp/send-contact", comController.sendContacts)
-    .post("/whatsapp/inte-list", comController.sendRadioButtons)
-    .post("/whatsapp/send-img", comController.sendImageByLink)
-    .post("/whatsapp/upload", comController.uploadMedia)
-    .post("/whatsapp/send-doc", comController.sendDocumentMessage)
-
-    // facebook plugin
-    // .post("/messenger", facebook.events)
+  // whatsapp plugin
+  .post("/whatsapp/set-status", whatsappController.markMessageAsRead)
+  .post("/whatsapp/send-msg", whatsappController.sendTextMessage)
+  .post("/whatsapp/inte-btn", whatsappController.sendButtonsMessage)
+  .post("/whatsapp/send-contact", whatsappController.sendContacts)
+  .post("/whatsapp/inte-list", whatsappController.sendRadioButtons)
+  .post("/whatsapp/send-img", whatsappController.sendImageByLink)
+  .post("/whatsapp/upload", whatsappController.uploadMedia)
+  .post("/whatsapp/send-doc", whatsappController.sendDocumentMessage);
 
 export default routes;
--- src/types/customRequest.ts
+++ src/types/customRequest.ts
@@ -2,11 +2,9 @@ import { Request } from "express";
 import { Types } from "mongoose";
 
 export interface User {
-    _id: Types.ObjectId;
-    email: string;
-    company: string;
+  sub: Types.ObjectId | string;
 }
 
 export interface CustomRequest extends Request {
-    user?: User;
+  user?: User;
 }
