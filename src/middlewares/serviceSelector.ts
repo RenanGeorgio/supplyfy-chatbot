8c09f9ffaae8f07512e308a9711fe1969232afa0
RenanGeorgio <regeo9@hotmail.com>
2024-04-13 21:14:09 -0300

    up

--- src/middlewares/serviceSelector.ts
+++ src/middlewares/serviceSelector.ts
@@ -22,16 +22,18 @@ const serviceSelectorMiddleware = async (req: CustomRequest, res: Response, next
                     next();
                     break;
                 case 'instagram':
+                    //instagramService(); OLD
                     next();
                     break;
                 case 'facebook':
                     next();
                     break;
                 case 'email':
+                    //emailService(); OLD
                     next();
                     break;
                 case 'telegram':
-		    //telegramService();
+		    //telegramService(); OLD
                     next();
                     break;
                 case 'cadastro':
