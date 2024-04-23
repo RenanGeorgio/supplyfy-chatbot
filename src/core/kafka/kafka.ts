e24157294bd23f7916ae169cef4e8ba025327d69
ThomasAFG <thomasafg@hotmail.com>
2024-04-23 14:21:03 -0300

    kafka brokers discover

--- src/core/kafka/kafka.ts
+++ src/core/kafka/kafka.ts
@@ -1,6 +1,24 @@
 import { Kafka, KafkaConfig } from 'kafkajs';
 
-const kafkaConfig: KafkaConfig = { brokers: [process.env.KAFKA_BROKERS || ""] }
-const kafka = new Kafka(kafkaConfig)
+const kafka = new Kafka({
+    clientId: 'my-app',
+    brokers: async () => {
+        const clusterResponse = await fetch(KAFKA_REST_URL, {
+            headers: 'application/vnd.api+json',
+        }).then(response => response.json())
+        const clusterUrl = clusterResponse.data[0].links.self
+
+        const brokersResponse = await fetch(`${clusterUrl}/brokers`, {
+            headers: 'application/vnd.api+json',
+        }).then(response => response.json())
+
+        const brokers = brokersResponse.data.map(broker => {
+            const { host, port } = broker.attributes
+            return `${host}:${port}`
+        })
+
+        return brokers
+    }
+})
 
 export default kafka;
