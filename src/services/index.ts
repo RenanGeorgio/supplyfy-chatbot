6c9d39909970d4d0b6cf7e769705b4742d6e17b0
RenanGeorgio <regeo9@hotmail.com>
2024-04-27 16:43:19 -0300

    fix
    fix to force reverse merger

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,35 +1,63 @@
-import whatsappWebService from "./whatsapp-web";
 import { listAllBots } from "../repositories/bot";
 import { telegramServiceController } from "./telegram";
 import { emailServiceController } from "./email";
 import { instagramServiceController } from "./instagram";
-import { socketServiceController } from "./socket";
+
+import emailService from "./email/listener";
+//import emailService from "./email"; original
+import instagramService from "./instagram";
+import { telegramServiceController } from "./telegram";
+import { socketServiceController } from "./socket"; // SAMUEL
+//import telegramService from "./telegram"; original
+import whatsappWebService from "./whatsapp-web";
 import { webhookPromiseHandler } from "./webhook/webhookHandler";
-import WhatsappService from "./whatsapp";
+
+import { messengerServiceController } from "./facebook";
+import whatsappWebService from "./whatsapp-web";
+import "./queue";
+import { IWebhook } from "../types";
+import { getWebhook } from "../repositories/webhook";
+
+// import ChatService from "./chatSocket";
+import { listAllBots } from "../repositories/bot";
+// import { telegramServiceController } from "./telegram"; OLD
 
 (async () => {
   const bots = await listAllBots();
   for (const bot of bots) {
+    const webhook = await getWebhook({ companyId: bot.companyId } as any);
     // inicializar os serviços quando o servidor iniciar ?
-
-    // if (bot.services?.telegram) {
-    //   await telegramServiceController.start(bot.services.telegram);
-    // }
-    // if (bot.services?.email) {
-    //   emailServiceController.start(bot.services.email);
-    // }
+    if (bot.socket) {
+      socketServiceController.start(bot.socket as any, webhook as IWebhook);
+    }
+    if (bot?.services?.telegram) {
+      telegramServiceController.start(bot.services.telegram as any, webhook as IWebhook);
+    }
+    if (bot?.services?.email) {
+      emailServiceController.start(bot.services.email as any,webhook as IWebhook);
+    }
     // if(bot.services?.instagram) {
     //   instagramServiceController.start(bot.services.instagram);
     // }
+    if (bot?.services?.facebook) {
+      messengerServiceController.start(bot.services.facebook, webhook as IWebhook);
+    }
   }
 })();
+
+// whatsappWebService("1");
 
 const servicesActions = {
   telegram: telegramServiceController,
   email: emailServiceController,
   instagramWeb: instagramServiceController,
-  socket: socketServiceController
+  socket: socketServiceController,
+  facebook: messengerServiceController,
   // whastapp
 };
 
-export { whatsappWebService, servicesActions, webhookPromiseHandler, WhatsappService };
+export { whatsappWebService, servicesActions, webhookPromiseHandler };
+*/
+
+// export { emailService, instagramService, telegramService, whatsappWebService }; original
+export { emailService, instagramService, telegramServiceController, whatsappWebService };
