be3f941ff67d88ec9aab4e7c4e8611f22aed63c3
ThomasAFG <thomasafg@hotmail.com>
2024-05-09 17:23:23 -0300

    Build 09-05-2024

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.MONGO_URL ?? "");
+mongoose.connect(process.env.MONGO_URL ?? "").then(() => { console.log("Database connected!") }).catch((err) => {console.log(err.message)});
 mongoose.Promise = global.Promise;
 
 export default mongoose;
