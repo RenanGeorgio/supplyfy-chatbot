01f9d276eb9c93057e4f54aa1f329718b0d8b600
Thomás de Abreu Fortuozo Gouvêa <thomasafg@hotmail.com>
2024-05-09 17:42:29 -0300

    Update redis.ts

--- src/core/redis.ts
+++ src/core/redis.ts
@@ -3,8 +3,8 @@ import * as redis from 'redis';
 const redisClient = redis.createClient({
   password: process.env.REDIS_PASSWORD,
   socket:{
-    host: process.env.REDIS_HOST,
-    port: Number(process.env.REDIS_PORT),
+    host: process.env.REDIS_HOST ?? "redis",
+    port: Number(process.env.REDIS_PORT ?? 6379),
   },    
 });
 
