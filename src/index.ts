bd78c56a0793efd6f22220f24cc0fb130140dfe6
ThomasAFG <thomasafg@hotmail.com>
2024-05-10 13:44:50 -0300

    Build 10-05-2024

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 8000;
 const HOST = process.env.HOST || "http://localhost";
 
 console.log("APP_SECRET: " + process.env.APP_SECRET);
+console.log("MONGO_URL: " + process.env.MONGO_URL);
 
 serverHttp.listen(PORT, () => {
     console.log(`Server running on ${HOST}:${PORT}`);
