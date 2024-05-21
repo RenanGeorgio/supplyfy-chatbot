387f3dfc5e557901e50714de4510dde236973110
ThomasAFG <thomasafg@hotmail.com>
2024-05-21 17:28:06 -0300

    Build 21-05-2024

--- src/webhooks/index.ts
+++ src/webhooks/index.ts
@@ -1,9 +1,11 @@
 import fbWebhookRouter from "./facebook-webhook";
 import igWebhookRouter from "./instagram-webhook";
 import waWebhookRouter from "./whatsapp-webhook";
+import webhookRouter from "./webhook";
 
 export {
     fbWebhookRouter,
     igWebhookRouter,
-    waWebhookRouter
+    waWebhookRouter,
+    webhookRouter
 }
