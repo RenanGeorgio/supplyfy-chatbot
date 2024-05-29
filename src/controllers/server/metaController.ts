9acf61caf61ce57dfd4f9ca2d9df4b3a074b41a2
Samuel <samuelmarques96@live.com>
2024-05-29 12:59:51 -0300

    feat

--- src/controllers/server/metaController.ts
+++ src/controllers/server/metaController.ts
@@ -1,46 +1,75 @@
 import { Response, NextFunction } from "express";
 import { CustomRequest } from "../../types";
+import { createOrUpdateAuthData } from "../../repositories/meta";
+
+const companyId = "1"; // mock
 
 export const save = async (req: CustomRequest, res: Response, next: NextFunction) => {
-    try {
-        const { accessToken, expiresIn, reauthorize_required_in, signedRequest, userID } = req.body;
-        // SALVAR
-        return res.status(200).send({ message: "Server is OK!" });
-    } catch (error) {
-        next(error);
+  try {
+    const { accessToken, expiresIn, reauthorize_required_in, signedRequest, userID} = req.body;
+
+    // alguma forma de associar ao companyId
+    const create = await createOrUpdateAuthData({
+      companyId,
+      accessToken,
+      expiresIn,
+      reauthorizeRequiredIn: reauthorize_required_in,
+      signedRequest,
+      userId: userID
+    });
+
+    if (!create) {
+      return res.status(400).send({ message: "Error" });
     }
+
+    return res.status(200).send({ message: "Server is OK!" });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const chageCode = async (req: CustomRequest, res: Response, next: NextFunction) => {
-    const { appId, redirectUri, code } = req.body;
-    
-    try {
-        const changeResponse = await fetch(`https://graph.facebook.com/v19.0/oauth/access_token?
+  const { appId, redirectUri, code } = req.body;
+
+  try {
+    const changeResponse =
+      await fetch(`https://graph.facebook.com/v19.0/oauth/access_token?
             client_id=${appId}
             &redirect_uri=${redirectUri}
             &client_secret=${process.env.APP_SECRET}
-            &code=${code}`
-        );
+            &code=${code}`);
 
-        if (changeResponse) {
-            const response = await fetch(`https://graph.facebook.com/v19.0/oauth/access_token?
+    if (changeResponse) {
+      const changResData = await changeResponse.json(); // SALVAR
+      const response =
+        await fetch(`https://graph.facebook.com/v19.0/oauth/access_token?
                 grant_type=fb_exchange_token
                 &client_id=${appId}
                 &client_secret=${process.env.APP_SECRET}
-                &fb_exchange_token=${changeResponse.body.access_token}`
-            );
+                &fb_exchange_token=${changResData.access_token}`);
 
-            const { access_token, token_type, expires_in } = response.body; // SALVAR
+      const { access_token, token_type, expires_in } = await response.json(); // SALVAR
 
-            const pageResponse = await fetch(`https://graph.facebook.com/v19.0/${appId}/accounts?access_token=${access_token}`);
+      if(access_token){
+        await createOrUpdateAuthData({ 
+          companyId, 
+          accessToken: access_token, 
+          expiresIn: expires_in, 
+          tokenType: token_type 
+        })
+      }
 
-            const { data, paging } = pageResponse.body; // SALVAR
+      const pageResponse = await fetch(
+        `https://graph.facebook.com/v19.0/${appId}/accounts?access_token=${access_token}`
+      );
 
-            return res.status(200).send({ changeResponse.body.access_token });
-        }
-            
-        return res.status(400).send({ message: "Problem to obtain token" });
-    } catch (error) {
-        next(error);
+      const { data, paging } = await pageResponse.json(); // SALVAR
+
+      return res.status(200).send({ access_token: changResData.access_token });
     }
+
+    return res.status(400).send({ message: "Problem to obtain token" });
+  } catch (error) {
+    next(error);
+  }
 };
