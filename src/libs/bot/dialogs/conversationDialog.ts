c927a4bcc09f6411e2ceb0bcc6fc509bca2ec98d
RenanGeorgio <43246444+RenanGeorgio@users.noreply.github.com>
2024-09-10 15:41:44 -0300

    Update conversationDialog.ts

--- src/libs/bot/dialogs/conversationDialog.ts
+++ src/libs/bot/dialogs/conversationDialog.ts
@@ -28,8 +28,10 @@ export class ConversationDialog extends CancelAndHelpDialog {
         } else {
             return await stepContext.next(chatDetails.travelDate);
         }
+        
+        return await stepContext.beginDialog(CHAT_DIALOG);
     }
-       return await stepContext.beginDialog(CHAT_DIALOG);
+       
     /**
      * Complete the interaction and end the dialog.
      */
