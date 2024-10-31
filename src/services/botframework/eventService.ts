8e8b8d215b8e92dcac458a9141e7c59f68bf7e28
RenanGeorgio <regeo9@hotmail.com>
2024-10-31 18:01:15 -0300

    up

--- src/services/botframework/eventService.ts
+++ src/services/botframework/eventService.ts
@@ -1,17 +1,49 @@
 import { sendFacebookTextMessage } from "../../controllers/com/facebook/facebookController";
-import { INSTAGRAM_MSG_TYPE } from "../../controllers/com/instagram/consumer";
-import { sendInstagramTextMessage } from "../../controllers/com/instagram/instagramController/data";
 import { sendInstagramMessage } from "../../controllers/com/service";
 import { sendWaTextMessage } from "../../controllers/com/whatsapp/whatsappController";
-import { WaMsgMetaData } from "../../types";
+import { Obj, WaMsgMetaData } from "../../types";
 import { Platforms } from "../../types/enums";
+import { enqueue } from "../enqueue";
+import { ENQUEUE_STATUS } from "../enqueue/constants/enqueue.enums";
+import { EventdataType, QueueAgentDTO } from "../enqueue/constants/enqueue.types";
 
+const CurrentEvent = class<EventdataType> {
+    CallSid: string | undefined
+    Caller: string | undefined 
+    From: string | undefined 
+    To: string | undefined 
+    QueuePosition: number | string | undefined
+    QueueSid: string | undefined
+    CurrentQueueSize: number | undefined
+    MaxQueueSize?: number | string
+    channel?: Platforms
+    constructor(maxSize: number = 100) {
+      this.MaxQueueSize = maxSize;
+    }
+  
+    setCurrentEvent({ CallSid, Caller, From, To, QueuePosition, QueueSid, CurrentQueueSize, channel }: EventdataType) {
+        this.CallSid = CallSid;
+        this.Caller = Caller;
+        this.From = From;
+        this.To = To;
+        this.QueuePosition = QueuePosition;
+        this.QueueSid = QueueSid
+        this.CurrentQueueSize = CurrentQueueSize;
+        this.channel = channel;
+    }
+
+    getCurrentEvent() {
+        return this;
+    }
+  };
 
 export default async function eventService(data: any) {
     try {
         const { result } = data;
         
         const channel = result.value.channel;
+
+        const currentEvent = new CurrentEvent();
 
         switch (channel) {
             case Platforms.WHATSAPP:
@@ -21,36 +53,73 @@ export default async function eventService(data: any) {
                     recipientName: result.value.name,
                     recipientPhoneNumberId: result.value.to,
                     accessToken: result.value.token,
-                }
+                };
+
+                const whatsappEvent: EventdataType = { 
+                    CallSid: result.conversation.id, 
+                    Caller: result.from.name, 
+                    From: result.from.id, 
+                    To: result.value.to, 
+                    QueuePosition: -1, 
+                    QueueSid: "", 
+                    CurrentQueueSize: -1, 
+                    channel: Platforms.WHATSAPP
+                 }
+
+                currentEvent.setCurrentEvent(whatsappEvent);
+
 
                 sendWaTextMessage(result.text, whatsappData);
                 break;
             case Platforms.INSTAGRAM:
-                if (result.value.type === INSTAGRAM_MSG_TYPE.PRIVATEREPLY) {
-                    const type = result.value.key;
+                const instagramEvent: EventdataType = { 
+                    CallSid: result.conversation.id, 
+                    Caller: result.from.name, 
+                    From: result.from.id, 
+                    To: result.value.to, 
+                    QueuePosition: -1, 
+                    QueueSid: "", 
+                    CurrentQueueSize: -1, 
+                    channel: Platforms.WHATSAPP
+                 }
 
-                    const requestBody = {
-                        recipient: {
-                          [type]: result.value.objectId,
-                        },
-                        message: result.text,
-                        tag: "HUMAN_AGENT",
-                    };
+                currentEvent.setCurrentEvent(instagramEvent);
 
-                    sendInstagramMessage(requestBody);
-                } else {
-                    const responses = sendInstagramTextMessage(result.value.senderID, result.text);
-
-                    sendInstagramMessage(responses);
-                }
+                sendInstagramMessage(responses);
                 break;
             case Platforms.FACEBOOK:
+                const facebookEvent: EventdataType = { 
+                    CallSid: result.conversation.id, 
+                    Caller: result.from.name, 
+                    From: result.from.id, 
+                    To: result.value.to, 
+                    QueuePosition: -1, 
+                    QueueSid: "", 
+                    CurrentQueueSize: -1, 
+                    channel: Platforms.WHATSAPP
+                 }
+
+                currentEvent.setCurrentEvent(facebookEvent);
+
                 sendFacebookTextMessage(result.value.senderID, result.text);
                 break;
             default:
                 console.log('Tipo de atendimento desconhecido.');
                 break;
         }
+
+        const eventToSend: Obj = currentEvent.getCurrentEvent();
+
+        const newData: QueueAgentDTO = {
+            eventData: eventToSend as any,
+            filterCompanyId: result.value.company,
+            filterQueueId: result.value.queue,
+            status: ENQUEUE_STATUS.QUEUED,
+            deQueuedTime: undefined,
+            queuedTime: new Date().toString()
+        }
+
+        enqueue(newData);
 
         return result
     } catch (error: any) {
